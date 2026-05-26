67c7f70e197e04e1bfee901727a173e08b4daf3a
Jair Meza <jair3202@gmail.com>
2026-05-26 13:49:55 -0400

    fix db string parsing

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,15 +4,8 @@ import * as schema from "./schema";
 
 const databaseUrl = process.env.DATABASE_URL;
 
-if (!databaseUrl) {
-  throw new Error("DATABASE_URL is required");
-}
-
-const connectionUrl = new URL(databaseUrl);
-connectionUrl.searchParams.delete("sslmode");
-
 const pool = new pg.Pool({
-  connectionString: connectionUrl.toString(),
+  connectionString: databaseUrl,
   ssl: { rejectUnauthorized: false },
 });
 
